f0d773a82497464f28c3465d3f83cbe0e3555702
Taylor Holberton <taylor.holberton@protonmail.com>
2021-11-25 15:19:05 -0500

    Added noise type specifier

--- viewer/main.cpp
+++ viewer/main.cpp
@@ -33,6 +33,93 @@ updateViewport(GLFWwindow* window)
 void
 randomize(FastNoiseLite& noise, Terrain& terrain);
 
+const char*
+toString(FastNoiseLite::NoiseType noiseType)
+{
+  switch (noiseType) {
+    case FastNoiseLite::NoiseType_OpenSimplex2:
+      return "OpenSimplex2";
+    case FastNoiseLite::NoiseType_OpenSimplex2S:
+      return "OpenSimplex2S";
+    case FastNoiseLite::NoiseType_Cellular:
+      return "Cellular";
+    case FastNoiseLite::NoiseType_Perlin:
+      return "Perlin";
+    case FastNoiseLite::NoiseType_ValueCubic:
+      return "ValueCubic";
+    case FastNoiseLite::NoiseType_Value:
+      return "Value";
+  }
+
+  return "";
+}
+
+int
+toIndex(FastNoiseLite::NoiseType noiseType)
+{
+  switch (noiseType) {
+    case FastNoiseLite::NoiseType_OpenSimplex2:
+      return 0;
+    case FastNoiseLite::NoiseType_OpenSimplex2S:
+      return 1;
+    case FastNoiseLite::NoiseType_Cellular:
+      return 2;
+    case FastNoiseLite::NoiseType_Perlin:
+      return 3;
+    case FastNoiseLite::NoiseType_ValueCubic:
+      return 4;
+    case FastNoiseLite::NoiseType_Value:
+      return 5;
+  }
+
+  return -1;
+}
+
+bool
+ImGui_NoiseType(FastNoiseLite::NoiseType* noiseType, ImGuiComboFlags flags = 0)
+{
+  const char* current = toString(*noiseType);
+
+  const char* items[]{
+    "OpenSimplex2", "OpenSimplex2S", "Cellular", "Perlin", "ValueCubic", "Value",
+  };
+
+  int currentIndex = -1;
+
+  for (int i = 0; i < 6; i++) {
+    if (strcmp(items[i], current) == 0) {
+      currentIndex = i;
+      break;
+    }
+  }
+
+  if (ImGui::Combo("NoiseType", &currentIndex, items, 6)) {
+    switch (currentIndex) {
+      case 0:
+        *noiseType = FastNoiseLite::NoiseType_OpenSimplex2;
+        break;
+      case 1:
+        *noiseType = FastNoiseLite::NoiseType_OpenSimplex2S;
+        break;
+      case 2:
+        *noiseType = FastNoiseLite::NoiseType_Cellular;
+        break;
+      case 3:
+        *noiseType = FastNoiseLite::NoiseType_Perlin;
+        break;
+      case 4:
+        *noiseType = FastNoiseLite::NoiseType_ValueCubic;
+        break;
+      case 5:
+        *noiseType = FastNoiseLite::NoiseType_Value;
+        break;
+    }
+    return true;
+  }
+
+  return false;
+}
+
 } // namespace
 
 int
@@ -85,8 +172,9 @@ main(int argc, char** argv)
   int genTerrainHeight = 1023;
 
   int randomizeSeed = 0;
-
   FastNoiseLite fastNoiseLite;
+  FastNoiseLite::NoiseType noiseType = FastNoiseLite::NoiseType_Perlin;
+  fastNoiseLite.SetNoiseType(noiseType);
 
   float metersPerPixel = 10;
 
@@ -149,6 +237,9 @@ main(int argc, char** argv)
 
     if (ImGui::SliderInt("Randomize Seed", &randomizeSeed, 0, 0x3fffffff))
       fastNoiseLite.SetSeed(randomizeSeed);
+
+    if (ImGui_NoiseType(&noiseType))
+      fastNoiseLite.SetNoiseType(noiseType);
 
     if (ImGui::Button("Randomize") && terrain)
       randomize(fastNoiseLite, *terrain);
